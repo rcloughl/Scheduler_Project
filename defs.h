0eed2be9b04041878f1685ea95161ac64ae509bd
Robert Cloughley <101719997+rcloughl@users.noreply.github.com>
2023-02-16 14:44:40 -0500

    i will code circles around you little boy

---
+++ defs.h
@@ -0,0 +1,22 @@
+/*****************************************************************
+*       defs.h - simplified for CPSC405 Lab by Gusty Cooper, University of Mary Washington
+*       adapted from MIT xv6 by Zhiyi Huang, hzy@cs.otago.ac.nz, University of Otago
+********************************************************************/
+
+struct proc;
+
+// proc.c
+int             Exit(int);
+int             Fork(int);
+int             Kill(int);
+void            pinit(void);
+void            procdump(void);
+void            scheduler(void);
+void 		lcfs_scheduler(void);
+void		mlfq_scheduler(void);
+int             userinit(void);
+int             Wait(int);
+int             Sleep(int, int);
+void            Wakeup(int);
+int 		nice_to_weight(int);
+
